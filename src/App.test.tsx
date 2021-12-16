adaccbc227c96da58c27bb880d484f951c4a52e0
riccjohn <riccjohn@gmail.com>
2021-12-16 10:18:40 -0600

    WIP: Create Nextjs app

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders a page title", () => {
-  render(<App />);
-  const title = screen.getByText(/Lycanthrope/i);
-  expect(title).toBeInTheDocument();
-});
