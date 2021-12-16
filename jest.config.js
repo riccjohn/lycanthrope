1bad3b25de84a7c479e104aa9c916700ce836271
riccjohn <riccjohn@gmail.com>
2021-12-16 11:29:06 -0600

    Install prettier

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
-  }
+  },
 }
 
 module.exports = createJestConfig(customJestConfig)
